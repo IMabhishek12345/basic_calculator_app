f59809ca022dadbe909f0a6ff1656315ce99a7d3
abhishek pokhriyal <abhishek.pokhriyal8@gmail.com>
2023-11-14 01:03:45 +0530

    created a basic function calculator

--- index.js
+++ index.js
@@ -5,29 +5,74 @@ const dataEquals=document.querySelector("[data-equals]");
 const deleteBtn=document.querySelector("[data-delete]");
 const currOperand=document.querySelector("[data-current-operand]");
 const prevOperand=document.querySelector("[data-previous-operand]");
-                            
-
+const dataOutputOperand=document.querySelector("[data-output-operand]");
+let result, op1,op2;
+let operator;
+console.log(result);
 console.log(numberBtn);
 
 numberBtn.forEach(num=>num.addEventListener("click",(e)=>{
  
  if (e.target.innerText==="." && currOperand.innerHTML.includes(".") ) return
- 
-   currOperand.innerHTML+=e.target.innerText;
- 
+  // currOperand.innerHTML+=e.target.innerText;
+  
+ if (result!==undefined){
+    currOperand.innerHTML+=e.target.innerText;
+  }else{
+    currOperand.innerHTML=e.target.innerText;
+  }  
+
 }))
 
 operationBtn.forEach(op=>op.addEventListener("click",(e)=>{
 
-        if (prevOperand!=""){
+        if (prevOperand!==""){
+          operator=e.target.innerText;
           currOperand.innerHTML+=e.target.innerText;
-         }
-         prevOperand.innerHTML=currOperand.innerHTML;
-         currOperand.innerHTML="";
-
+        }
+        if(result===undefined){
+          prevOperand.innerHTML=currOperand.innerHTML;
+          currOperand.innerHTML="";
+        }else{
+          currOperand.innerHTML="";
+          prevOperand.innerHTML= result+operator;
+        }     
+          
+        
 }))
 
-
+allclearBtn.addEventListener("click",()=>{
+  prevOperand.innerHTML="";
+  currOperand.innerHTML="";
+  dataOutputOperand.innerHTML="";
+  result=undefined;
+  })
 deleteBtn.addEventListener("click",()=>{
-        currOperand.innerHTML="";
+     currOperand.innerHTML=currOperand.innerText.slice(0,-1);
+})
+
+dataEquals.addEventListener("click",(e)=>{
+// console.log(prevOperand.innerText.slice(0,-1));
+   op1=Number(prevOperand.innerText.slice(0,-1));
+   op2=Number(currOperand.innerText);  
+
+switch(operator){
+        case "+":
+          result= op1+op2;    
+          break
+        case "-":
+          result= op1-op2;
+          break
+        case "*":
+          result=op1*op2;     
+          break
+        case "/":
+          result= op1/op2;
+          break
+        
+        default:
+           return  
+        }   
+      // currOperand.innerHTML=result;
+      dataOutputOperand.innerHTML=result;
 })
